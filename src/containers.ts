d902937ad1932d3084bbafa359688aad28cdc98b
dinhoSiwa <claudiosilva.one@gmail.com>
2025-11-23 05:46:45 -0300

    feat: add-portfolio-service-and-repository-to-di-container

--- src/containers.ts
+++ src/containers.ts
@@ -1,7 +1,10 @@
 // src/container.ts
-import { FormService } from './services/FormService';
-import { PrismaServiceFormRepository } from './repositories/implementations/PrismaServiceFormRepository';
-import type { IServiceFormRepository } from './repositories/interfaces/IserviceFormsRepositoty';
+import { FormService } from './services/service-form/FormService';
+import { PrismaServiceFormRepository } from './repositories/PrismaServiceFormRepository';
+import type { IServiceFormRepository } from './interfaces/service-forms/IserviceFormsRepositoty';
+import type { IStoragePortifolioRepository } from './interfaces/portifolio-upload/IStorageRepository';
+import { CloudinaryRepository } from './repositories/CloudinaryRepository ';
+import { PortfolioUploadService } from './services/portifolio-upload/portifolioUploadService';
 
 class Container {
   private instances = new Map();
@@ -21,8 +24,17 @@ class Container {
 
 export const container = new Container();
 
+
+// Configuração dos Services de Formulário
 const formRepository: IServiceFormRepository = new PrismaServiceFormRepository();
 const formService = new FormService(formRepository);
 
-container.register('IFormRepository', formRepository);
+// Configuração dos Services de Upload de Portfólio
+const portfolioUploadRepository: IStoragePortifolioRepository = new CloudinaryRepository();
+const portfolioUploadService = new PortfolioUploadService(portfolioUploadRepository);
+
+container.register('IServiceFormRepository', formRepository);
 container.register('FormService', formService);
+
+container.register('IStoragePortifolioRepository', portfolioUploadRepository);
+container.register('PortfolioUploadService', portfolioUploadService);
